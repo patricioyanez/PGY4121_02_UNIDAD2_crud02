17b4f2bcb3652aac000ec42b6a775157e1c0d99f
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-09-27 16:20:24 -0300

    guardar form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,16 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  palabra : string;
+  mensaje : string;
   constructor() {}
 
   ngOnInit()
   {
+    /*
     console.log("Hola");
+    // permite guardar un dato en el localStorage
     localStorage.setItem("1", "Mi primer valor");
 
+    // permite rescatar un dato en el localStorage por el Id
     const x = localStorage.getItem("1");
     console.log(x);
+    */
   }
 
+  guardar(nombre: HTMLInputElement)
+  {
+    const nom = nombre.value; //recupera la info escrita por el usuario
+
+    if(nom.trim().length >= 3)
+    {
+      let id = localStorage.length + 1
+      localStorage.setItem(id.toString(), nom);
+      this.mensaje = "Guardado con éxito: " + nom;
+      nombre.value = "";
+    }
+
+  }
 }
